aa68f4d1b97c7594ebdc7d5b8d86b4159de7d3bf
Kilian Hendrickx <kilian.hendrickx@ugent.be>
2014-05-08 15:16:04 +0200

    getDistance

--- filters.h
+++ filters.h
@@ -6,27 +6,27 @@
 using namespace cv;
 using namespace std;
 	int iLowH = 19;
-    int iHighH = 146;
+    int iHighH = 124;
 
     int iLowS = 87; 
-    int iHighS = 107;
+    int iHighS = 50;
 
     int iLowV = 124;
-    int iHighV = 191;
+    int iHighV = 89;
 	
 	double iHighGreen = sqrt(iHighH*iHighH + iHighS*iHighS + iHighV*iHighV);
 	double iLowGreen = sqrt(iLowH*iLowH + iLowS*iLowS + iLowV*iLowV);
 
 	
-double getMean(const Mat &imgOrignal){
+double getDistance(const Mat &imgOrignal, double origH, double origS, double origV){
 
 	vector<Mat> channels;
 	split(imgOrignal, channels);
-    Scalar h = mean(channels[0]);
-    Scalar s = mean(channels[1]);
-    Scalar v = mean(channels[2]);
+    double h = (mean(channels[0])[0]) - origH;
+    double s = (mean(channels[1])[0]) - origS;
+    double v = (mean(channels[2])[0]) - origV;
 	
-	return sqrt(h[0]*h[0]+s[0]*s[0]+v[0]*v[0]);
+	return sqrt(h*h+s*s+v*v);
 }
 
 void greenFilter(const Mat &imgOriginal, Mat &output){
@@ -45,10 +45,21 @@ void greenFilter(const Mat &imgOriginal, Mat &output){
     Mat imgHSV;
     cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from ?? to HSV
     //Mat imgThresholded;
-	printf("%f\n", getMean(imgHSV));
+	output = imgHSV;
 	
 	
-    inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), output); //Threshold the image => enkel groen over houden
+	printf("%f\n", getDistance(imgHSV, iHighH,iHighS,iHighV));
+	if (getDistance(imgHSV, iHighH,iHighS,iHighV)>50){
+		vector<Mat> channels;
+		split(imgHSV, channels);
+		double h = (mean(channels[0])[0]);
+		double s = (mean(channels[1])[0]);
+		double v = (mean(channels[2])[0]);
+		output.setTo(cv::Scalar(h,s,v));
+		cvtColor(output, output, COLOR_HSV2BGR);
+	}
+	
+    // inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), output); //Threshold the image => enkel groen over houden
 	
     dilate( output, output, getStructuringElement(MORPH_RECT, Size(3, 3)) ); //dilate the image to get rid of holes
     //cvtColor(output,output,CV_RGB2GRAY);
