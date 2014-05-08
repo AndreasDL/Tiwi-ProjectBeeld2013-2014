9e568b455143c045dfe3faef3e237dd771c473a5
Kilian Hendrickx <kilian.hendrickx@ugent.be>
2014-05-08 14:27:34 +0200

    getMean van mat

--- filters.h
+++ filters.h
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <math.h>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
 using namespace cv;
 using namespace std;
-int iLowH = 19;
+	int iLowH = 19;
     int iHighH = 146;
 
     int iLowS = 87; 
@@ -12,9 +13,24 @@ int iLowH = 19;
 
     int iLowV = 124;
     int iHighV = 191;
+	
+	double iHighGreen = sqrt(iHighH*iHighH + iHighS*iHighS + iHighV*iHighV);
+	double iLowGreen = sqrt(iLowH*iLowH + iLowS*iLowS + iLowV*iLowV);
+
+	
+double getMean(const Mat &imgOrignal){
+
+	vector<Mat> channels;
+	split(imgOrignal, channels);
+    Scalar h = mean(channels[0]);
+    Scalar s = mean(channels[1]);
+    Scalar v = mean(channels[2]);
+	
+	return sqrt(h[0]*h[0]+s[0]*s[0]+v[0]*v[0]);
+}
 
 void greenFilter(const Mat &imgOriginal, Mat &output){
-
+	
     namedWindow("Control",CV_WINDOW_AUTOSIZE); //create a window called "Control"
     //Create trackbars in "Control" window
     cvCreateTrackbar("LowH", "Control", &iLowH, 255);
@@ -29,8 +45,11 @@ void greenFilter(const Mat &imgOriginal, Mat &output){
     Mat imgHSV;
     cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from ?? to HSV
     //Mat imgThresholded;
-
+	printf("%f\n", getMean(imgHSV));
+	
+	
     inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), output); //Threshold the image => enkel groen over houden
+	
     dilate( output, output, getStructuringElement(MORPH_RECT, Size(3, 3)) ); //dilate the image to get rid of holes
     //cvtColor(output,output,CV_RGB2GRAY);
 }
