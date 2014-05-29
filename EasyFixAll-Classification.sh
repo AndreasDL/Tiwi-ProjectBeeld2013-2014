649a3456da5973e54ef8248f9c2c5630f9775776
adelille <andreasdelille@hotmail.com>
2014-05-30 00:27:28 +0200

    final fix linuxé

--- EasyFixAll-Classification.sh
+++ EasyFixAll-Classification.sh
@@ -6,15 +6,21 @@ RESULTS_NAME="test.txt";
 SVM_PATH="SVM_BIN_LINUX_64bit/svm_perf_classify";
 
 #cleanup
-rm featurevectors.dat; #the results are appended to this file
+rm SVM/featurevectors.dat; #the results are appended to this file
 rm $RESULTS_NAME;
 
 #create new featurevector
-echo "creating feature vector";
+echo "creating feature vectors for each frame";
 ./bin/example $MOVIE_PATH;
 
 
 #classify
+mv featurevectors.dat SVM;
+cd SVM;
 echo "classifiying this might take a while";
-perl SVM/svmClassifierHeen.pl $SVM_PATH "featurevectors.dat" | tee $RESULTS_NAME; #heenweg
+perl svmClassifierHeen.pl $SVM_PATH "featurevectors.dat" | tee $RESULTS_NAME; #heenweg
 #perl SVM/svmClassifierTerug.pl $SVM_PATH "featurevectors.dat" | tee $RESULTS_NAME; #heenweg
+
+rm tmp_dat.dat;
+rm tmp_predictions;
+cd ..;
